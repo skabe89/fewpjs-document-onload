5cf62546bf9535b44e5ad65031d056ad02e5b290
Sean <skabe89@gmail.com>
2021-02-12 15:09:48 -0500

    commit tree a089bec1c8dc80ab7c9b60b9065c5ad9fa2bd24b

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+  let d = document.getElementById("text");
+  d.textContent = "This is really cool!";
+}); 
+
+console.log("This should come first")
